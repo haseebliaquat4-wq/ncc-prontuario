6e85a1edc651429ec21e827b9061898477fde341
haseebliaquat4-wq <haseebliaquat4@gmail.com>
2026-06-15 08:37:37 +0200

    Add service worker for caching network requests

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'ncc-v1';
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(networkResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
